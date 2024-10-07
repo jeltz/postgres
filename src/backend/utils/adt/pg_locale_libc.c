d2e36eb180eebdcf6dbe751d657925b5c43ab47c
Jeff Davis <jeff@j-davis.com>
2024-10-07 12:51:27 -0700

    Remove provider field from pg_locale_t.
    The behavior of pg_locale_t is entirely specified by methods, so a
    separate provider field is no longer necessary.

--- src/backend/utils/adt/pg_locale_libc.c
+++ src/backend/utils/adt/pg_locale_libc.c
@@ -495,7 +495,6 @@ create_pg_locale_libc(Oid collid, MemoryContext context)
 	loc = make_libc_collator(collate, ctype);
 
 	result = MemoryContextAllocZero(context, sizeof(struct pg_locale_struct));
-	result->provider = COLLPROVIDER_LIBC;
 	result->deterministic = true;
 	result->collate_is_c = (strcmp(collate, "C") == 0) ||
 		(strcmp(collate, "POSIX") == 0);
@@ -615,8 +614,6 @@ strncoll_libc(const char *arg1, ssize_t len1, const char *arg2, ssize_t len2,
 	const char *arg2n;
 	int			result;
 
-	Assert(locale->provider == COLLPROVIDER_LIBC);
-
 	if (bufsize1 + bufsize2 > TEXTBUFLEN)
 		buf = palloc(bufsize1 + bufsize2);
 
@@ -670,8 +667,6 @@ strnxfrm_libc(char *dest, size_t destsize, const char *src, ssize_t srclen,
 	char	   *buf = sbuf;
 	size_t		bufsize = srclen + 1;
 	size_t		result;
-
-	Assert(locale->provider == COLLPROVIDER_LIBC);
 
 	if (srclen == -1)
 		return strxfrm_l(dest, src, destsize, locale->info.lt);
@@ -781,7 +776,6 @@ strncoll_libc_win32_utf8(const char *arg1, ssize_t len1, const char *arg2,
 	int			r;
 	int			result;
 
-	Assert(locale->provider == COLLPROVIDER_LIBC);
 	Assert(GetDatabaseEncoding() == PG_UTF8);
 
 	if (len1 == -1)
