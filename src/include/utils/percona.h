5ac375d558f19f2479e851db9fe5e687fb9b52eb
Andreas Karlsson <andreas.karlsson@percona.com>
2025-04-25 15:53:14 +0200

    Remove unused return value from check_percona_api_version()
    There is no user of this return value and furthermore the function can
    only ever return true.

--- src/include/utils/percona.h
+++ src/include/utils/percona.h
@@ -14,15 +14,13 @@
 
 extern const PGDLLIMPORT int percona_api_version;
 
-static inline bool check_percona_api_version(void)
+static inline void
+check_percona_api_version(void)
 {
-    if (PERCONA_API_VERSION != percona_api_version)
-    {
-        elog(FATAL, "Percona API version mismatch, the extension was built againts a different PostgreSQL version!");
-        return false;
-    }
-
-    return true;
+	if (PERCONA_API_VERSION != percona_api_version)
+	{
+		elog(FATAL, "Percona API version mismatch, the extension was built against a different PostgreSQL version!");
+	}
 }
 
-#endif // PERCONA__H__
+#endif
